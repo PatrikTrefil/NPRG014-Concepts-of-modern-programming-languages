0f8457e1ba0db8d41a9697e72e53ff91bf94ed30
Patrik Trefil <patrik.trefil@gmail.com>
2023-10-16 09:23:13 +0200

    exercises lecture 9.10.

--- lecture1009/exercises/04_dynamism/G04_DynamicObjectCreation.groovy
+++ lecture1009/exercises/04_dynamism/G04_DynamicObjectCreation.groovy
@@ -9,12 +9,14 @@ interface Calculator {
 
     def increment(a)
 }
-
-final myCalculator = [
+final obj = [
         add: {a, b -> a + b},
         multiply: {a, b -> a * b},
-        increment: {it + 1}
-] as Calculator
+        increment: {it -> add(it, 1)},
+        subtract: {a, b -> a - b}
+];
+obj.increment.delegate = obj;
+final myCalculator = obj as Calculator
 
 assert 10 == myCalculator.add(3, 7)
 assert 6 == myCalculator.multiply(2, 3)
@@ -22,7 +24,7 @@ assert 6 == myCalculator.increment(5)
 
 //TASK uncomment and see the reported exception
 //TASK add the subtract method so that the following line passes
-//assert 3 == myCalculator.subtract(4, 1)
+assert 3 == myCalculator.subtract(4, 1)
 
 //TASK Re-implement the increment() method so that it reuses the code of add()
 
